471f5bf0f3cfc86a65649b2a7db2f179f4498739
GaneshJampa <91380384+GaneshJampa@users.noreply.github.com>
2021-12-13 14:50:51 +0530

    updated  Fahrenheit Script

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
-$(document).ready(function(){
-    var myButton = document.getElementById('button');
+function fahrenheit(){
     var value;
     var urlcel;
-    var myButton = document.getElementById('button');
-    myButton.onclick = function(){
+           value = document.getElementById("city").value;
+             urlcel="https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid=679a1ed886ed6c003e3e37adecc4d85e";
+    $.ajax({ 
+      url: urlcel
+    }).done(function( data ){
+        var f='&deg;'+"F";
+        $(".temp").html(data.main.temp)
+        $(".min-temp").html(data.main.temp_min)
+        $(".max-temp").html(data.main.temp_max)
+        $(".units").html(f)
+    });
+}
+
+function celsius(){
+    var value;
+    var urlcel;
            value = document.getElementById("city").value;
              urlcel="https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid=679a1ed886ed6c003e3e37adecc4d85e&units=metric";
-            var cel = document.getElementById('celsius');
-            cel.onclick = function(){
-                 urlcel="https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid=679a1ed886ed6c003e3e37adecc4d85e&units=metric";
-            }
-            var fah = document.getElementById('fahrenheit');
-            fah.onclick = function(){
-                 urlcel="https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid=679a1ed886ed6c003e3e37adecc4d85e";
-            }
     $.ajax({ 
       url: urlcel
     }).done(function( data ){
-        console.log(data)
-        var td=data.dt;
-        const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-        var date = new Date(td*1000);
-        $(".day-main").html(weekday[date.getDay()])
-        $(".time").html(date.getHours()+":"+date.getMinutes())
-        $(".sunrise").html((new Date(data.sys.sunrise*1000)).getHours()+":"+(new Date(data.sys.sunrise*1000)).getMinutes())
-        $(".sunset").html((new Date(data.sys.sunset*1000)).getHours()+":"+(new Date(data.sys.sunset*1000)).getMinutes())
-        $(".cityname").html(data.name)
+      var f='&deg;'+"C";
         $(".temp").html(data.main.temp)
         $(".min-temp").html(data.main.temp_min)
         $(".max-temp").html(data.main.temp_max)
-        $(".wind").html(data.wind.speed)
-        $(".wdesc").html(data.weather[0].description)
-        $(".wmain").html(data.weather[0].main)
-        $(".humidity").html(data.main.humidity)
-        $(".visibility").html(data.visibility)
+        $(".units").html(f)
     });
 }
-var f = document.getElementById('fahrenheit');
-    f.onclick = function(){
+$(document).ready(function(){
+    $("#search").click(function(){
+    var value;
+    var urlcel;
            value = document.getElementById("city").value;
-             urlcel="https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid=679a1ed886ed6c003e3e37adecc4d85e";console.log(urlcel)
+             urlcel="https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid=679a1ed886ed6c003e3e37adecc4d85e&units=metric";
     $.ajax({ 
       url: urlcel
     }).done(function( data ){
@@ -49,20 +44,30 @@ var f = document.getElementById('fahrenheit');
         var date = new Date(td*1000);
         $(".day-main").html(weekday[date.getDay()])
         $(".time").html(date.getHours()+":"+date.getMinutes())
-        $(".sunrise").html((new Date(data.sys.sunrise*1000)).getHours()+":"+(new Date(data.sys.sunrise*1000)).getMinutes())
-        $(".sunset").html((new Date(data.sys.sunset*1000)).getHours()+":"+(new Date(data.sys.sunset*1000)).getMinutes())
-        $(".cityname").html(data.name)
+        $("#sunrise").html((new Date(data.sys.sunrise*1000)).getHours()+":"+(new Date(data.sys.sunrise*1000)).getMinutes())
+        $("#sunset").html((new Date(data.sys.sunset*1000)).getHours()+":"+(new Date(data.sys.sunset*1000)).getMinutes())
+        $("#cityname").html(data.name)
         $(".temp").html(data.main.temp)
         $(".min-temp").html(data.main.temp_min)
         $(".max-temp").html(data.main.temp_max)
-        $(".wind").html(data.wind.speed)
-        $(".wdesc").html(data.weather[0].description)
-        $(".wmain").html(data.weather[0].main)
-        $(".humidity").html(data.main.humidity)
-        $(".visibility").html(data.visibility)
+        $("#wind").html(data.wind.speed)
+        $("#wdesc").html(data.weather[0].description)
+        $("#wmain").html(data.weather[0].main)
+        if(data.main.humidity<=30)
+        $(".quality-humidity").html("Good Quality")
+        else if(data.main.humidity<=60)
+       $(".quality-humidity").html("Average Quality")
+       else
+       $(".quality-humidity").html("Bad Quality")
+        $("#humidity").html(data.main.humidity)
+        if(data.main.visibility<=4000)
+        $(".quality-visibility").html("Bad")
+        else if(data.main.humidity<=10000)
+       $(".quality-visibility").html("Average")
+       else
+       $(".quality-visibility").html("Good")
+        $("#visibility").html(data.visibility)
     });
-}
-
-
+    });
 })
 
